4b37b0e023d9d8cd77e118199df0109abcc6927c
LDVargas <47862630+LDVargas@users.noreply.github.com>
2020-04-01 20:59:13 -0600

    archivos nuevos

---
+++ PrimerProyecto/cmake-build-debug/CMakeFiles/PrimerProyecto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrimerProyecto.dir/main.c.obj"
+  "PrimerProyecto.exe"
+  "PrimerProyecto.exe.manifest"
+  "PrimerProyecto.pdb"
+  "libPrimerProyecto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrimerProyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
