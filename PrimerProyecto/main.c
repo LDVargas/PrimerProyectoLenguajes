e341ffb8b980e604c72c3c75bd70ecf04a82374f
LDVargas <47862630+LDVargas@users.noreply.github.com>
2020-04-02 21:37:45 -0600

    trabajo 02042020

--- PrimerProyecto/main.c
+++ PrimerProyecto/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *punteroArchivo = NULL;
+    punteroArchivo = fopen("F:/Universidad/Semestre 11/Lenguajes/Proyectos/PrimerProyect/PrimerProyectoLenguajes/archivo.txt","r");
+
+    if(punteroArchivo == NULL){
+        printf("No se abrio el archivo");
+        return -1;
+    }
+
+    fclose(punteroArchivo);
     return 0;
 }
